b74b59816808a377dca4784f7773a800f583f8a6
Seron Chanderabose <144434600+SeronC@users.noreply.github.com>
2023-09-08 11:20:23 +0100

    copied from Gitea
    copy and pasted from local git pull

---
+++ testing/test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println("Program 1 - Quad A")
+	piscine.QuadA(5, 3)
+	piscine.QuadA(5, 1)
+	piscine.QuadA(1, 1)
+	piscine.QuadA(1, 5)
+
+	fmt.Println("Program 2 - Quad B")
+	piscine.QuadB(5, 3)
+	piscine.QuadB(5, 1)
+	piscine.QuadB(1, 1)
+	piscine.QuadB(1, 5)
+
+	fmt.Println("Program 3 - Quad C")
+	piscine.QuadC(5, 3)
+	piscine.QuadC(5, 1)
+	piscine.QuadC(1, 1)
+	piscine.QuadC(1, 5)
+
+	fmt.Println("Program 4 - Quad D")
+	piscine.QuadD(5, 3)
+	piscine.QuadD(5, 1)
+	piscine.QuadD(1, 1)
+	piscine.QuadD(1, 5)
+
+	fmt.Println("Program 5 - Quad E")
+	piscine.QuadE(5, 3)
+	piscine.QuadE(5, 1)
+	piscine.QuadE(1, 1)
+	piscine.QuadE(1, 5)
+}
